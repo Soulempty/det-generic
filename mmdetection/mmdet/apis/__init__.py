805e88e1429a96eaa01fcdabd5a48324254e2a34
焦超 <chao.jiao@aqrose.com>
2022-12-20 06:49:44 +0000

    Update __init__.py

--- mmdetection/mmdet/apis/__init__.py
+++ mmdetection/mmdet/apis/__init__.py
@@ -1,6 +1,6 @@
 #coding:utf-8
-from .test import TestInfo, Detect
-from .train import TrainInfo, Detection
-from .utils import nms, replace_ImageToTensor, single_gpu_test, multi_gpu_test
+from .train import Detection
+from .test import Detect
+from .utils import nms, replace_ImageToTensor, single_gpu_test, multi_gpu_test, data_split
 
-__all__ = ['TestInfo', 'Detect', 'TrainInfo', 'Detection','nms', 'replace_ImageToTensor', 'single_gpu_test','multi_gpu_test']
+__all__ = ['Detection','Detect', 'nms', 'replace_ImageToTensor', 'single_gpu_test','multi_gpu_test','data_split']
